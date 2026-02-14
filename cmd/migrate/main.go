a7d3eb85d1d4dfd5542339ed658e4b9a9c9b8d7a
Prohor722 <prohor.b722@gmail.com>
2026-02-14 23:51:24 +0600

    air environment setup

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -9,4 +9,12 @@ func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("Please provide a migration direction: 'up' or 'dowmn'")
 	}
+
+	direction := os.Args[1]
+
+	db, err := sql.Open("sqlite3", "./data.db")
+
+	if(err != nil) {
+		log.Fatal(err)
+	}
 }
